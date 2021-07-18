169668e3c2c755cf66a14481d50dc2c3c560c94f
qing <fk_qing@163.com>
2021-07-18 17:04:19 +0800

    first commit

---
+++ src/main/java/com/qing/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.qing.git;
+
+/**
+ * @author weiqing
+ * @description
+ * @create 2021-07-18 16:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
